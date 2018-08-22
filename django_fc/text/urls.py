e2c7e45ed894855437134f173e9bb4b7d5c32194
sergei <sergei.lushtak@gmail.com>
2018-08-21 22:45:39 -0700

    fixed strange problem with double shash in url (work_with_text//)

--- django_fc/text/urls.py
+++ django_fc/text/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='text'
 
 urlpatterns = [
-    url(r"^work_with_text/(?P<file_name>[^\"*]*)/", views.work_with_text, name="work_with_text"),
+    url(r"^work_with_text/(?P<file_name>[^\"*]*)", views.work_with_text, name="work_with_text"),
     url(r"^delete_file/(?P<file_name>[^\"*]+)/", views.delete_file, name="delete_file"),
     url(r"^save_file/(?P<file_name>[^\"*]+)/", views.save_file, name="save_file"),
 ]
